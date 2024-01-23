87b966f7b8f22ec209a398ca647a060612f8225b
Alena Sharai <exultantislupus@gmail.com>
2024-01-23 18:13:25 +0400

    feat: solve first task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import fs from 'fs';
+import path from 'path';
+
 const create = async () => {
-    // Write your code here 
+  try {
+    fs.open('./files/fresh.txt', (err) => {
+      if (err) {
+        if (err.code === 'ENOENT') {
+          fs.writeFile('./files/fresh.txt', 'I am fresh and young', (err) => {
+            if (err) console.log(err.message);
+          });
+        } else console.log(err.message);
+      } else throw Error('FS operation failed');
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 await create();
