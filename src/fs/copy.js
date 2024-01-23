3d1f14f456d846c63a340719cc394e40f557178d
Alena Sharai <exultantislupus@gmail.com>
2024-01-23 18:25:30 +0400

    feat: solve second task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,20 @@
+import fs from 'fs';
+
 const copy = async () => {
-    // Write your code here 
+  fs.open('./files_copy', (err) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        fs.cp('./files', './files_copy', { recursive: true }, (err) => {
+          if (err) {
+            if (err.code === 'ENOENT') throw Error('FS operation failed');
+            else {
+              console.log(err.message);
+            }
+          }
+        });
+      } else console.log(err.message);
+    } else throw Error('FS operation failed');
+  });
 };
 
 await copy();
