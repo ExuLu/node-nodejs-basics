c83c427c570659904d978498a43653f3388cad4a
Alena Sharai <exultantislupus@gmail.com>
2024-01-24 18:41:25 +0400

    feat: solve third task

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,21 @@
+import fs from 'fs';
+
 const rename = async () => {
-    // Write your code here 
+  fs.open('./files/properFilename.md', (err) => {
+    if (!err) throw Error('FS operation failed');
+    else {
+      fs.rename(
+        './files/wrongFilename.txt',
+        './files/properFilename.md',
+        (err) => {
+          if (err) {
+            if (err.code === 'ENOENT') throw Error('FS operation failed');
+            else console.log(err.message);
+          }
+        }
+      );
+    }
+  });
 };
 
 await rename();
