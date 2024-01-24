21da5f51342d954ac266276b5c2d931117080ccc
Alena Sharai <exultantislupus@gmail.com>
2024-01-24 18:45:09 +0400

    feat: solve forth task

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,12 @@
+import fs from 'fs';
+
 const remove = async () => {
-    // Write your code here 
+  fs.unlink('./files/fileToRemove.txt', (err) => {
+    if (err) {
+      if (err.code === 'ENOENT') throw Error('FS operation failed');
+      else console.log(err.message);
+    }
+  });
 };
 
 await remove();
