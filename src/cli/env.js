247829feb9e77e5b8a5abb5a97c32f941ad6ede8
Alena Sharai <exultantislupus@gmail.com>
2024-01-25 16:39:49 +0400

    feat: sove env task

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+  const envsRSS = Object.keys(process.env)
+    .filter((key) => key.includes('RSS'))
+    .map((key) => {
+      const value = process.env[key];
+      const string = `${key}=${value}`;
+      return string;
+    });
+  envsRSS.forEach((envValue) => console.log(envValue));
 };
 
 parseEnv();
