214f0246ee16cd7d75449456ed73606589c744d8
Alena Sharai <exultantislupus@gmail.com>
2024-01-25 16:30:37 +0400

    feat: solve args task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+  const argArray = process.argv.slice(2);
+  const arrayWithPairs = argArray
+    .map((arg, index) => {
+      if (index % 2 === 0) {
+        const argName = arg.replaceAll('-', '');
+        const value = argArray[index + 1];
+        const newStr = `${argName} is ${value}`;
+        return newStr;
+      } else return null;
+    })
+    .filter((el) => el !== null);
+  arrayWithPairs.forEach((el) => console.log(el));
 };
 
 parseArgs();
