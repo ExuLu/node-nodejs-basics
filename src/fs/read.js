61dc3f624592817ddc87ab3a4b35459cb34b9700
Alena Sharai <exultantislupus@gmail.com>
2024-01-25 16:17:17 +0400

    feat: solve sixth task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,10 @@
+import fs from 'fs';
+
 const read = async () => {
-    // Write your code here 
+  fs.readFile('./files/fileToRead.txt', (err, data) => {
+    if (err) throw Error('FS operation failed');
+    else console.log(data.toString());
+  });
 };
 
 await read();
