b109931357766b200925337fb8bf0ccb0040edb9
Alena Sharai <exultantislupus@gmail.com>
2024-01-25 19:20:45 +0400

    fix: add absolute path to folders and file in create and copy tasks

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,10 +1,15 @@
 import fs from 'fs';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const copy = async () => {
-  fs.open('./files_copy', (err) => {
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const originalDirPath = path.join(__dirname, 'files');
+  const copyDirPath = path.join(__dirname, 'files_copy');
+  fs.open(copyDirPath, (err) => {
     if (err) {
       if (err.code === 'ENOENT') {
-        fs.cp('./files', './files_copy', { recursive: true }, (err) => {
+        fs.cp(originalDirPath, copyDirPath, { recursive: true }, (err) => {
           if (err) {
             if (err.code === 'ENOENT') throw Error('FS operation failed');
             else {
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,19 +1,19 @@
 import fs from 'fs';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const create = async () => {
-  try {
-    fs.open('./files/fresh.txt', (err) => {
-      if (err) {
-        if (err.code === 'ENOENT') {
-          fs.writeFile('./files/fresh.txt', 'I am fresh and young', (err) => {
-            if (err) console.log(err.message);
-          });
-        } else console.log(err.message);
-      } else throw Error('FS operation failed');
-    });
-  } catch (err) {
-    console.log(err);
-  }
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const newFilePath = path.join(__dirname, 'files', 'fresh.txt');
+  fs.open(newFilePath, (err) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        fs.writeFile(newFilePath, 'I am fresh and young', (err) => {
+          if (err) console.log(err.message);
+        });
+      } else console.log(err.message);
+    } else throw Error('FS operation failed');
+  });
 };
 
 await create();
