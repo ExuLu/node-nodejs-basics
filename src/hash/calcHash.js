508ec340166c1f461f6474760ace6583aeef39af
Alena Sharai <exultantislupus@gmail.com>
2024-01-29 14:43:12 +0400

    reafactor: change console.log to process.stdout in calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,7 +1,7 @@
-// import sha256 from 'sha256';
 import { createHash } from 'crypto';
-import { createReadStream, createWriteStream } from 'fs';
+import { createReadStream } from 'fs';
 import { dirname, join } from 'path';
+import { stdout } from 'process';
 import { fileURLToPath } from 'url';
 
 const calculateHash = async () => {
@@ -12,7 +12,7 @@ const calculateHash = async () => {
   readStream.on('data', (chunk) => (data += chunk));
   readStream.on('end', () => {
     const hash = createHash('sha256').update(data).digest('hex');
-    console.log(hash);
+    stdout.write(hash);
   });
 };
 
