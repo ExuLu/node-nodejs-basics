d10726de848d05aae20602c5ef46176e0f7cbf5a
Alena Sharai <exultantislupus@gmail.com>
2024-01-29 17:25:10 +0400

    feat: solve compress and decompress tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { createGzip } from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const filePath = join(__dirname, 'files', 'fileToCompress.txt');
+  const zipPath = join(__dirname, 'files', 'archive.gz');
+  const readStream = createReadStream(filePath, 'utf-8');
+  const writeStream = createWriteStream(zipPath);
+  const gzip = createGzip();
+
+  readStream.pipe(gzip).pipe(writeStream);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,17 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { createUnzip } from 'zlib';
+
 const decompress = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const filePath = join(__dirname, 'files', 'fileToCompress.txt');
+  const zipPath = join(__dirname, 'files', 'archive.gz');
+  const readStream = createReadStream(zipPath);
+  const writeStream = createWriteStream(filePath, 'utf-8');
+  const unzip = createUnzip();
+
+  readStream.pipe(unzip).pipe(writeStream);
 };
 
 await decompress();
