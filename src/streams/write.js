0755ed7b2bce1951e9ca8d746cec26672f8ff125
Alena Sharai <exultantislupus@gmail.com>
2024-01-29 14:47:50 +0400

    feat: solve streams write task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import { createWriteStream } from 'fs';
+import { dirname, join } from 'path';
+import { stdin } from 'process';
+import { fileURLToPath } from 'url';
+
 const write = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const writeFilePath = join(__dirname, 'files', 'fileToWrite.txt');
+  const writeStream = createWriteStream(writeFilePath, 'utf-8');
+  stdin.on('data', (data) => {
+    writeStream.write(data);
+    process.exit();
+  });
 };
 
 await write();
