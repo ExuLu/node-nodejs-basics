b2b95b1d84709c090ff66b6e80bd376ad893dc24
Alena Sharai <exultantislupus@gmail.com>
2024-01-29 14:41:54 +0400

    feat: solve streams read task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import { createReadStream } from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { stdout } from 'process';
+
 const read = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const readFilePath = join(__dirname, 'files', 'fileToRead.txt');
+  const stream = createReadStream(readFilePath, 'utf-8');
+  let data = '';
+  stream.on('data', (chunk) => (data += chunk));
+  stream.on('end', () => stdout.write(data));
 };
 
 await read();
