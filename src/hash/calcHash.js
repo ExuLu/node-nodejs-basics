53411f8280f6db2bc31cef7b44879b77acd4cef9
Alena Sharai <exultantislupus@gmail.com>
2024-01-29 14:36:45 +0400

    feat: solve calcHash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+// import sha256 from 'sha256';
+import { createHash } from 'crypto';
+import { createReadStream, createWriteStream } from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const filePath = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+  let data = '';
+  const readStream = createReadStream(filePath, 'utf-8');
+  readStream.on('data', (chunk) => (data += chunk));
+  readStream.on('end', () => {
+    const hash = createHash('sha256').update(data).digest('hex');
+    console.log(hash);
+  });
 };
 
 await calculateHash();
