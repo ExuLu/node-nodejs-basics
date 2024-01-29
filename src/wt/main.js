801e6b4649dfe5ea159a03544e6c8a93411e0beb
Alena Sharai <exultantislupus@gmail.com>
2024-01-29 18:53:32 +0400

    feat: solve wt task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,29 @@
+import { Worker } from 'worker_threads';
+import { cpus } from 'os';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
 const performCalculations = async () => {
-    // Write your code here
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const workerPath = join(__dirname, './worker.js');
+
+  const cores = cpus();
+  const coresNumber = cores.length;
+
+  const promises = [];
+  for (let i = 0; i < coresNumber; i++) {
+    promises.push(
+      new Promise((resolve) => {
+        const worker = new Worker(workerPath, { workerData: i + 10 });
+        worker.on('message', (message) => resolve(message));
+        worker.on('error', (err) => {
+          resolve('something');
+        });
+      })
+    );
+  }
+
+  Promise.all(promises).then((data) => console.log(data));
 };
 
 await performCalculations();
